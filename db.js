c1d8aa355bdaa217298d67101e420cbc6a3c7a2e
Laura <laurasarabiaf@gmail.com>
2021-03-12 10:06:35 -0300

    agrega citas

---
+++ db.js
@@ -0,0 +1,99 @@
+const Sequelize = require('sequelize');
+
+// acá creamos la conexión a la Base de Datos
+const sql = new Sequelize('doctor', 'root', 'laurasarabia', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+const User = sql.define('User', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'Debe indicar un nombre'
+      },
+      len: {
+        args: [2],
+        msg: 'El nombre debe ser de largo al menos 2'
+      }
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'Debe indicar un email'
+      },
+      len: {
+        args: [3],
+        msg: 'El email debe ser de largo al menos 3'
+      },
+      isEmail: {
+        msg: 'Debe ser un email válido'
+      }
+    }
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'Debe indicar una contraseña'
+      },
+      len: {
+        args: [3],
+        msg: 'La contraseña debe ser de largo al menos 3'
+      },
+    }
+  },
+});
+
+
+const Cita = sql.define('Cita', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  date: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'Debe escribir un Cita'
+      }
+    }
+  }, 
+  time: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  complain: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }  
+});
+
+User.hasMany(Cita);
+Cita.belongsTo(User);
+
+//  después sincronizamos nuestro código con la base de datos
+sql.sync()
+.then(() => {
+  console.log('Base de datos y tablas creadas');
+}); 
+
+// finalmente acá listamos todos los modelos que queremos exportar
+module.exports = {
+  User,
+  Cita
+};
+
---
+++ routes/routes.js
@@ -0,0 +1,65 @@
+/**
+ * Acá dejamos las rutas internas (rutas que asumen que el usuario ya se logueo a nuestra App)
+ */
+const { Router } = require('express');
+const router = Router();
+const {User, Cita} = require('../db')
+
+const citas = 
+[
+  {
+      date: "01-20-2021",
+      time: "12:20",
+      complain: "aaaaaaaaaa aaaa"
+  }   
+]
+
+
+// Middleware: Verifica si el usuario está logueado.
+// en caso de que no, lo mandamos al login
+function checkLogin(req, res, next) {
+  if (req.session.user == null) {
+    res.redirect('/login');
+  }
+  res.locals.user = req.session.user;
+  /* si llegamos hasta acá, esntonces estamos seguros
+   que si existe req.session.user */
+  next();
+}
+
+
+router.get('/', checkLogin, async (req, res) => {  
+  const citas = await Cita.findAll({
+    include: [{model: User}]
+  });
+  res.render('index.ejs', {citas});
+});
+
+
+//redirigir a crear cita
+router.get('/vistaCrear', checkLogin, async (req, res) => {  
+  res.render('agendar.ejs');
+});
+
+//para mostrar la nueva cita
+router.post("/crear",  async (req, res) => {
+  let nuevaAgenda = await Cita.create({    
+    date: req.body.date,
+    time: req.body.time,
+    complain: req.body.complain,
+    UserId: req.body.UserId
+  });   
+  console.log(nuevaAgenda);
+  res.redirect('/');
+});
+
+router.get('/eliminar/:id', async (req,res) => {
+  console.log("Elminarrrrrr");
+  //configurar para que solo elimine el paciente 
+  const eliminar = await Cita.findByPk(req.params.id);
+  await eliminar.destroy();
+
+  res.redirect("/")
+});
+
+module.exports = router;
